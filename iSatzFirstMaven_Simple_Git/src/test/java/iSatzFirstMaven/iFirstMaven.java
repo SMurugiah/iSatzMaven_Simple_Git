ce4c0ef76f1d7af1dc019c7c3c756ff1ce0d80cf
SathishMurugiah <SathishMurugiah@DESKTOP-5L9D1ND.T-mobile.com>
2018-05-07 23:14:04 -0400

    Simple Maven Project

---
+++ iSatzFirstMaven_Simple_Git/src/test/java/iSatzFirstMaven/iFirstMaven.java
@@ -0,0 +1,30 @@
+package iSatzFirstMaven;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class iFirstMaven {
+
+	@Test (priority = 0)
+	public void SampleTestScript7() {
+		System.out.println("Test Case Number 7");
+	}
+  
+	@Test (priority = 1)
+	public void SampleTestScript5() {
+		System.out.println("Test Case Number 5");
+		Assert.assertEquals("TestCase5", "TestCase5");
+	}
+	
+	@Test (priority = 2)
+	public void SampleTestScript1 () {
+		System.out.println("Test Case Number 1");
+		Assert.assertEquals("TestCase1", "TestCase1");
+	}
+	
+	@Test (priority = 3)
+	public void SampleTestScript3 () {
+		System.out.println("Test Case Number 3");
+		Assert.assertEquals("Google", "Google");
+	}
+}
